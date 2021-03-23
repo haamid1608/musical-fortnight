e3b857de2d8d26c1043884229f3fa9c83af32dba
Haamid <haamidmajoria.hm@gmail.com>
2021-03-23 17:09:57 +0530

    Added multiple lines

--- my-dummy-proj/src/Hello.java
+++ my-dummy-proj/src/Hello.java
@@ -3,5 +3,9 @@ public class Hello {
 
 	public void method1() {
 		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+		System.out.println("line5");
 	}
 }
